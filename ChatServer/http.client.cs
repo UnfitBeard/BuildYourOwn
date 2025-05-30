ddd62f8489686840b54cdd6e4b4fae8eb9dbac18
Mr.Nobody <njunge995@gmail.com>
2025-05-30 18:54:14 +0300

    Bugs bugs and bugs.......its baaad

---
+++ ChatServer/http.client.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Chat Server!");
+        }
+    }
+}
