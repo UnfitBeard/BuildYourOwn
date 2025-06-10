7bbc91cfd42e6627855654dadcf0a1bf2f7c8eff
Mr.Nobody <njunge995@gmail.com>
2025-06-10 14:01:40 +0300

    Branching

--- ChatServer/obj/Debug/net8.0/ChatServer.AssemblyInfo.cs
+++ ChatServer/obj/Debug/net8.0/ChatServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+196527aa68b2a13ceec8571173f2642bbcb650ce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6979f8f2d1420dc6544d0d330c60eab86fc7edf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Git/git.js
+++ Git/git.js
@@ -2,7 +2,10 @@
 function Git(name) {
   this.name = name;
   this.lastCommitId = -1;
-  this.HEAD = null; // reference to the last commit
+  var master = new Branch('master', null)
+  this.HEAD = master; // initialize a repo with default set to master
+  this.branches = []
+  this.branches.push(master)
 }
 
 // Commit -- when commits are chained together they form the history of your projects
@@ -15,16 +18,21 @@ function Commit(id, parent, message) {
   // Assume that this has a change property
 }
 
+function Branch(name, commit) {
+  this.name = name;
+  this.commit = commit
+}
+
 //Adding the ability to git to create a commit
 Git.prototype.commit = function (message) {
   var commit = new Commit(++this.lastCommitId, this.HEAD, message);
-  this.HEAD = commit; // Update head and current branch
+  this.HEAD.commit = commit; // Update head and current branch
   return commit;
 };
 
 Git.prototype.log = function () {
   //Start from the HEAD
-  var commit = this.HEAD;
+  var commit = this.HEAD.commit;
   var history = []; // array of commits in reverse order
 
   // Start from the last commit
@@ -38,6 +46,23 @@ Git.prototype.log = function () {
 
   return history;
 };
+
+Git.prototype.checkout = function (branchName) {
+  for (var i = this.branches.length; i++;) {
+    if (this.branches[i] === branchName) {
+      // We found an existing branch
+      console.log("Switched to existing branch: " + branchName);
+      this.HEAD === this.branches[i]
+      return this;
+    }
+    // If no branch with the name
+    var branch = new Branch(branchName, this.HEAD.commit)
+    this.branches.push(branch)
+    this.HEAD = branch
+    console.log("Switched to new Branch: " + branchName)
+    return this
+  }
+}
 
 // Branches
 export default Git;
