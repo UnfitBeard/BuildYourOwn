6779d84f51e840fefb14a2ba525f854f19c46024
Mr.Nobody <njunge995@gmail.com>
2025-06-25 09:06:15 +0300

    ReadMe file

--- ChatServer/obj/Debug/net8.0/ChatServer.AssemblyInfo.cs
+++ ChatServer/obj/Debug/net8.0/ChatServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99122cf5f6453d2edbf433764daa75c7169aed21")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+287ed8092acdff0f0f820b024d1e836058a2d489")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
